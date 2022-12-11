fd3049990a51bc4195ca937eabc45770b4144974
osita007 <osita00007@gmail.com>
2022-12-11 13:58:43 -0800

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - Entry
+ * main - print the last digit of the number stored in the variable n
  * Return: Always 0
  */
 int main(void)
@@ -28,3 +28,4 @@ int main(void)
 		printf("last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
 	return (0);
+}
