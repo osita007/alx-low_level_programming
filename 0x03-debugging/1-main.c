bc113505256dde85d0d34fc76ebe18e469feb944
osita007 <osita00007@gmail.com>
2022-12-15 21:44:02 -0800

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,31 +1,25 @@
-#include "main.h"
-
-
+#include <stdio.h>
 
 /**
+ *main - causes an infinite loop
  *
- *  * main - Test function for positive or negative
- *
- *   * Return: 0
- *
- *    */
-
-
+ * Return: 0
+ */
 
 int main(void)
-
 {
-
 	 int i;
 
+	  printf("Infinite loop incoming :(\n");
 
+	   i = 0;
 
-	  i = 0;
+	    /*while (i < 10)*/
+	    /*{*/
+	    /* putchar(i);*/
+	    /*}*/
 
-	   positive_or_negative(i);
+	    printf("Infinite loop avoided! \\o/\n");
 
-
-
-	   return (0);
-
+		return (0);
 }
